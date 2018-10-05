abb1022292b59da096ab5985fe1fb8aecd21b6d7
GeniusK <geniusk1047@naver.com>
2018-10-05 11:10:53 +0900

    simpleMessage 작업

---
+++ python/simpleMessage.sh
@@ -0,0 +1,1 @@
+./venv/bin/python ./simple_message.py
--- python/simple_message.py
+++ python/simple_message.py
@@ -4,33 +4,40 @@ import datetime
 import uuid
 import hmac
 import hashlib
+import configparser
 
-apiKey = 'NCSCRL5NEDD4GXN0'
-apiSecret = '2EKV5OEQESARHFHOQNOYLKMZVAZVTWW9'
+config = configparser.ConfigParser()
+config.read('config.ini')
+apiKey = config['AUTH']['ApiKey']
+apiSecret = config['AUTH']['ApiSecret']
 
 
-def uniqid():
+def unique_id():
     return str(uuid.uuid1().hex)
 
 
-def getISODateTime():
-    # calculate the offset taking into account daylight saving time
+def get_iso_datetime():
     utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
     utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
     return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
-    # return str(int(time.time()))
 
 
-def getSignature(key, msg):
-    print(hmac.new(key.encode(), msg.encode(), hashlib.md5).hexdigest())
-    return hmac.new(key.encode(), msg.encode(), hashlib.md5).hexdigest()
+def get_signature(key, msg):
+    return hmac.new(key.encode(), msg.encode(), hashlib.sha256).hexdigest()
 
 
 if __name__ == '__main__':
-    date = getISODateTime()
-    salt = uniqid()
+    date = get_iso_datetime()
+    salt = unique_id()
     msg = date + salt
-    res = requests.post('https://rest.coolsms.co.kr/messages/v4/send', headers={
-        'Authorization':
-    'HMAC-SHA256 ApiKey=' + apiKey + ', Date=' + date + ', salt=' + salt + ', signature=' + getSignature(apiSecret, msg)})
+    headers = {'Authorization': 'HMAC-SHA256 ApiKey=' + apiKey + ', Date=' + date + ', salt=' + salt + ', signature=' +
+                                get_signature(apiSecret, msg)}
+    data = {
+        'message': {
+            'to': config['VALUE']['to'],
+            'from': config['VALUE']['from'],
+            'text': 'test'
+        }
+    }
+    res = requests.post(config['SERVER']['URI'], headers=headers, json=data)
     print(res.text)
