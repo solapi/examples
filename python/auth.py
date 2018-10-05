e3a92abb7d03d2985c848560e8655c09761f2446
GeniusK <geniusk1047@naver.com>
2018-10-05 11:41:19 +0900

    auth 추가

---
+++ python/auth.py
@@ -0,0 +1,19 @@
+import time
+import datetime
+import uuid
+import hmac
+import hashlib
+
+
+def unique_id():
+    return str(uuid.uuid1().hex)
+
+
+def get_iso_datetime():
+    utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
+    utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
+    return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
+
+
+def get_signature(key, msg):
+    return hmac.new(key.encode(), msg.encode(), hashlib.sha256).hexdigest()
