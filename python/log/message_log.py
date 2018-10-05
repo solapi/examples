a60edc7b03b8bcfe16fef06d80cdc5b67d3dbcc0
GeniusK <geniusk1047@naver.com>
2018-10-05 14:22:40 +0900

    message_log 작업

---
+++ python/log/message_log.py
@@ -0,0 +1,14 @@
+import requests
+import configparser
+import auth
+import json
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+apiKey = config['AUTH']['ApiKey']
+apiSecret = config['AUTH']['ApiSecret']
+
+if __name__ == '__main__':
+    res = requests.get(config['SERVER']['URI'] + 'list',
+                       headers=auth.get_headers(apiKey, apiSecret))
+    print(json.loads(res.text))
