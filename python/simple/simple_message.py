f706c56bd082f11278e1f9af688455582bb18f4d
GeniusK <geniusk1047@naver.com>
2018-10-05 11:41:26 +0900

    auth 적용

--- python/simple/simple_message.py
+++ python/simple/simple_message.py
@@ -1,10 +1,6 @@
 import requests
-import time
-import datetime
-import uuid
-import hmac
-import hashlib
 import configparser
+import auth
 
 config = configparser.ConfigParser()
 config.read('./config.ini')
@@ -12,26 +8,12 @@ apiKey = config['AUTH']['ApiKey']
 apiSecret = config['AUTH']['ApiSecret']
 
 
-def unique_id():
-    return str(uuid.uuid1().hex)
-
-
-def get_iso_datetime():
-    utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
-    utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
-    return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
-
-
-def get_signature(key, msg):
-    return hmac.new(key.encode(), msg.encode(), hashlib.sha256).hexdigest()
-
-
 if __name__ == '__main__':
-    date = get_iso_datetime()
-    salt = unique_id()
+    date = auth.get_iso_datetime()
+    salt = auth.unique_id()
     msg = date + salt
     headers = {'Authorization': 'HMAC-SHA256 ApiKey=' + apiKey + ', Date=' + date + ', salt=' + salt + ', signature=' +
-                                get_signature(apiSecret, msg)}
+                                auth.get_signature(apiSecret, msg)}
     data = {
         'message': {
             'to': config['VALUE']['to'],
