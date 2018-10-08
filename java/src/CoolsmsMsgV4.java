66944d4779b176d6f8ae20b1ac8e0963c535b550
GeniusK <geniusk1047@naver.com>
2018-10-08 11:31:28 +0900

    createMessageGroup 작업

--- java/src/CoolsmsMsgV4.java
+++ java/src/CoolsmsMsgV4.java
@@ -1,4 +1,5 @@
 import model.request.Message;
+import model.response.GroupRes;
 import model.response.MessageRes;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -8,6 +9,9 @@ public interface CoolsmsMsgV4 {
     @POST("/messages/v4/send")
     Call<MessageRes> sendMessage(@Header("Authorization") String auth,
                                  @Body Message message);
+
+    @POST("/messages/v4/groups")
+    Call<GroupRes> createGroup(@Header("Authorization") String auth);
 
     @GET("/messages/v4/groups")
     Call<ResponseBody> getGroups(@Header("Authorization") String auth);
