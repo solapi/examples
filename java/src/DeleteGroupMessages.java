186e331f88277d7649208790d993a161fd1ecb6a
GeniusK <geniusk1047@naver.com>
2018-10-10 14:47:43 +0900

    DeleteGroupMessages.java 마무리

--- java/src/DeleteGroupMessages.java
+++ java/src/DeleteGroupMessages.java
@@ -3,18 +3,25 @@ import model.response.DeleteGroupModel;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DeleteGroupMessages {
     public static void main(String[] args) {
         ArrayList<String> messageIds = new ArrayList<>();
-        messageIds.add("M4V20181010104828WDK1PWF32SSIVQR");
-        Call<DeleteGroupModel> api = APIInit.getAPI().deleteGroupMessages(APIInit.getHeaders(), "G4V20181010104753WODJ8GIAFYJ93GH", new MessageIds(messageIds));
+        messageIds.add("[Message ID를 입력해주세요]");
+        Call<DeleteGroupModel> api = APIInit.getAPI().deleteGroupMessages(APIInit.getHeaders(), "[Group ID를 입력해주세요]", new MessageIds(messageIds));
         api.enqueue(new Callback<DeleteGroupModel>() {
             @Override
             public void onResponse(Call<DeleteGroupModel> call, Response<DeleteGroupModel> response) {
                 // 성공시 200이 출력됩니다.
                 System.out.println(response.code());
+                try {
+                    System.out.println(response.errorBody().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
