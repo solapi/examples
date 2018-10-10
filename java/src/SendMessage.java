ec01a2e58ee106e3315158a65b182abf7e50f3e1
GeniusK <geniusk1047@naver.com>
2018-10-10 14:17:47 +0900

    SendMessage.java 마무리

--- java/src/SendMessage.java
+++ java/src/SendMessage.java
@@ -6,14 +6,16 @@ import retrofit2.Response;
 
 public class SendMessage {
     public static void main(String[] args) {
-        Message message =  new Message("01029951047", "01029951047", "test");
+        Message message =  new Message("[수신번호를 입력하세요]", "[발신번호를 입력하세요]", "[수신번호를 입력하세요]");
         Call<MessageModel> api = APIInit.getAPI().sendMessage(APIInit.getHeaders(), message);
         api.enqueue(new Callback<>() {
             @Override
             public void onResponse(Call<MessageModel> call, Response<MessageModel> response) {
+                // 성공 시 200이 출력됩니다.
+                System.out.println("statusCode : " + response.code());
                 MessageModel body = response.body();
                 System.out.println("groupId : " + body.getGroupId());
-                System.out.println("getMessageId : " + body.getMessageId());
+                System.out.println("messageId : " + body.getMessageId());
                 System.out.println("to : " + body.getTo());
                 System.out.println("from : " + body.getFrom());
                 System.out.println("type : " + body.getType());
