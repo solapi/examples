40c34d7f8904794b9e2d797d5fcb3ef2d0ab2c03
GeniusK <geniusk1047@naver.com>
2018-12-11 17:51:19 +0900

    이미지 리스트 가져오기 추가

---
+++ python/image/getImageList.py
@@ -0,0 +1,22 @@
+import requests
+import base64
+import json
+import configparser
+import sys
+import os.path
+
+libdir = os.path.dirname(__file__)
+sys.path.append(os.path.split(libdir)[0])
+
+from auth import auth
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+apiKey = config['AUTH']['ApiKey']
+apiSecret = config['AUTH']['ApiSecret']
+
+if __name__ == '__main__':
+    # [IMAGE_ID] 에 이미지 아이디를 넣어주세요
+    res = requests.get(config['SERVER']['IMGURI'] + '/images',
+                        headers=auth.get_headers(apiKey, apiSecret))
+    print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
