69d37cf1fdbb48684d57ac2114b1b97d3a1f3318
GeniusK <geniusk1047@naver.com>
2018-12-11 14:11:52 +0900

    update delete group message

--- python/group/delete_group_message.py
+++ python/group/delete_group_message.py
@@ -1,7 +1,13 @@
 import requests
 import configparser
-import auth
 import json
+import sys
+import os.path
+
+libdir = os.path.dirname(__file__)
+sys.path.append(os.path.split(libdir)[0])
+
+from auth import auth
 
 config = configparser.ConfigParser()
 config.read('../config.ini')
@@ -14,6 +20,7 @@ if __name__ == '__main__':
     # ex) G4V20181005122748TESTTESTTESTTES
     data = {'messageIds':
         [
+            # 배열로 id를 입력하여 한 요청에 여러개의 메세지를 제거할 수 있습니다.
             '[INPUT_MESSAGE_ID]',
             '[INPUT_MESSAGE_ID]'
         ]
@@ -21,4 +28,4 @@ if __name__ == '__main__':
     res = requests.delete(config['SERVER']['URI'] + 'groups/[INPUT_GROUP_ID]/messages',
                           headers=auth.get_headers(apiKey, apiSecret),
                           json=data)
-    print(json.loads(res.text))
+    print(json.dumps(json.loads(res.text), indent=2))
