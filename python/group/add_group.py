dcf4cd7b993d2ad10e3f0a8a1a5df126d0397e28
GeniusK <geniusk1047@naver.com>
2018-12-11 12:27:42 +0900

    add_group 업데이트

--- python/group/add_group.py
+++ python/group/add_group.py
@@ -1,16 +1,20 @@
 import requests
 import configparser
-import auth
 import json
+import sys
+import os.path
+
+libdir = os.path.dirname(__file__)
+sys.path.append(os.path.split(libdir)[0])
+
+from auth import auth
 
 config = configparser.ConfigParser()
 config.read('../config.ini')
 apiKey = config['AUTH']['ApiKey']
 apiSecret = config['AUTH']['ApiSecret']
 
-
 if __name__ == '__main__':
     res = requests.post(config['SERVER']['URI'] + 'groups',
                         headers=auth.get_headers(apiKey, apiSecret))
-    print(json.loads(res.text))
-
+    print(json.dumps(json.loads(res.text), indent=2))
