7492f4fd10fd725c828743a67a466c48d7ec6e6d
GeniusK <geniusk1047@naver.com>
2018-12-12 16:19:24 +0900

    필요없는 주석 제거

--- python/image/getImageList.py
+++ python/image/getImageList.py
@@ -15,7 +15,6 @@ apiKey = config['AUTH']['ApiKey']
 apiSecret = config['AUTH']['ApiSecret']
 
 if __name__ == '__main__':
-    # [IMAGE_ID] 에 이미지 아이디를 넣어주세요
     res = requests.get(config['SERVER']['IMGURI'] + '/images',
                         headers=auth.get_headers(apiKey, apiSecret))
     print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
