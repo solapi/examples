8568b57b713188bca6fb6fa5bc761a3c5a5a8f20
GeniusK <geniusk1047@naver.com>
2019-03-25 15:49:52 +0900

    java example lms 고려사항 추가

--- java/src/model/request/Message.java
+++ java/src/model/request/Message.java
@@ -4,19 +4,29 @@ import com.google.gson.JsonObject;
 
 public class Message {
     private JsonObject message = new JsonObject();
-    // 주석처럼 추가로 parameter를 늘릴 수 있습니다.
-    public Message(String to, String from, String text/*, String type */) {
+    // SMS일 경우는 발신,수신 번호와 내용만 넣으시면 됩니다.
+    public Message(String to, String from, String text) {
         this.message.addProperty("to", to);
         this.message.addProperty("from", from);
         this.message.addProperty("text", text);
-//        this.message.addProperty("type", type);
     }
 
-    // 이미지 첨부 전송시 사용 (MMS일 경우)
-    public Message(String to, String from, String text, String imageId) {
+    // LMS일 경우 subject에 제목을 넣어주세요
+    public Message(String to, String from, String text, String subject) {
         this.message.addProperty("to", to);
         this.message.addProperty("from", from);
         this.message.addProperty("text", text);
+        this.message.addProperty("subject", subject);
+        this.message.addProperty("type", "LMS");
+    }
+
+    // MMS일 경우 imageId에 이미지 ID를 넣어주세요
+    public Message(String to, String from, String text, String subject, String imageId) {
+        this.message.addProperty("to", to);
+        this.message.addProperty("from", from);
+        this.message.addProperty("text", text);
+        this.message.addProperty("subject", subject);
         this.message.addProperty("imageId", imageId);
+        this.message.addProperty("type", "MMS");
     }
 }
