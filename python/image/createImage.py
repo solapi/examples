8ed7cd80c2b70280b274803dd84e9f99403dac9f
StevenMcguffin <45581453+StevenMcguffin@users.noreply.github.com>
2019-12-23 11:26:32 +0900

    [python + ruby] image -> storage

--- python/image/createImage.py
+++ python/image/createImage.py
@@ -17,5 +17,5 @@ apiSecret = config['AUTH']['ApiSecret']
 if __name__ == '__main__':
     # 이미지를 바꾸시려면 testImage.jpg 대신
     # 사용하실 이미지가 있는 파일 경로를 넣어주세요
-    res = image.createImage('./image/testImage.jpg', config['SERVER']['IMGURI'], auth.get_headers(apiKey, apiSecret))
+    res = image.createImage('./image/testImage.jpg', config['SERVER']['IMGURI'] + 'files', auth.get_headers(apiKey, apiSecret))
     print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
--- python/image/image.py
+++ python/image/image.py
@@ -1,13 +1,11 @@
 import requests
 import base64
 
-
 def createImage(image_path, image_url, headers):
     with open(image_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     data = {
-        'image': str(encoded_string)[2:-1]
+        'file': str(encoded_string)[2:-1],
+        'type': 'MMS'
     }
-    return requests.post(image_url + '/images',
-                         headers=headers,
-                         json=data)
+    return requests.post(image_url, headers=headers, json=data)
